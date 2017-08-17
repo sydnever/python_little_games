bb392f93a759ccddd199afe02a0fe3d8e19fea6c
Syd <324760805@qq.com>
2017-08-17 23:47:02 +0800

    create 1_1_jump_ball.py

---
+++ 1_1_jump_ball.py
@@ -0,0 +1,9 @@
+import os
+import sys
+if len(sys.argv) == 0:
+    print "1.1 jump_ball by syd"
+    print "usage: 1_1_jump_ball.py o=1 x=5 y=10"
+    print "o: "
+    print "     1. a hold ball"
+    print "x: the x coordinate"
+    print "y: the y coordinate"
