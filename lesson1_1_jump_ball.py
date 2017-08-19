31a5989c979d771de6ce2ef202865c3b89f694eb
Syd <324760805@qq.com>
2017-08-19 19:36:44 +0800

    just a little

--- lesson1_1_jump_ball.py
+++ lesson1_1_jump_ball.py
@@ -2,7 +2,7 @@
 """
     Lesson 1.1 A jump ball
 """
-import os
+# import os
 import sys
 
 
@@ -14,35 +14,56 @@ def print_help():
         help
     """
     print("1.1 jump_ball by syd")
-    print("usage: 1_1_jump_ball.py o=1 x=5 y=10")
+    print("usage: lessson1_1_jump_ball.py o=1 x=5 y=10")
     print("o: ")
     print("     1. a hold ball")
-    print("x: the x coordinate (5 default)")
-    print("y: the y coordinate (10 default)")
+    print("x: the x coordinate (5 default, 0 < x < 100, int)")
+    print("y: the y coordinate (10 default, 0 < y < 100, int)")
 
 
 if len(sys.argv) == 1:
     print_help()
     exit(1)
 
+# exit code
+EXIT_TOO_MANY_ARGVS = 5
+EXIT_INVALID_PARAM = 2
+EXIT_SUCCESS_END = 0
+
+# default value
 coordinate_x = 5
 coordinate_y = 10
 option_o = 1
 
+
 if len(sys.argv) > 4:
     print("too many argvs")
     print_help()
-    exit(5)
+    exit(EXIT_TOO_MANY_ARGVS)
 
-for tmp_argv in sys.argv:
-    if tmp_argv[0:1] == "o=":
+print(sys.argv)
+for tmp_argv in sys.argv[1:]:
+    print(tmp_argv[0:2])
+    if tmp_argv[0:2] == "o=":
         option_o = tmp_argv[2:]
-    elif tmp_argv[0:1] == "x=":
+    elif tmp_argv[0:2] == "x=":
         coordinate_x = tmp_argv[2:]
-    elif tmp_argv[0:1] == "y=":
+    elif tmp_argv[0:2] == "y=":
         coordinate_y = tmp_argv[2:]
     else:
         print("invalid param: " + tmp_argv)
-        exit(2)
+        exit(EXIT_INVALID_PARAM)
 
-print("test: o=" + option_o + " x=" + coordinate_x + " y=" + coordinate_x)
+
+if coordinate_x not in range(1, 100) or coordinate_y not in range(1, 100):
+    print("___________________________________________")
+    print("Error: please make x and y in a valid range")
+    print("*******************************************")    
+    print("test: o=" + option_o + " x=" + coordinate_x + " y=" + coordinate_x)    
+    exit(EXIT_INVALID_PARAM)
+else:
+    print("test: o=" + option_o + " x=" + coordinate_x + " y=" + coordinate_x)
+    
+
+
+exit(EXIT_SUCCESS_END)
--- test1_1.py
+++ test1_1.py
@@ -1,18 +1,41 @@
 #!/usr/bin/env python3
 import os
+import random
 
-TEST1_1 = "/Users/syd/PycharmProjects/python_little_games/1_1_jump_ball.py"
-print ("********** test begin **********")
 
-print ("1_1_jump_ball no params")
-os.system("python3 " + TEST1_1)
+def test(xy_l, xy_r, o_l, o_r):
+    RAND_O = str(random.randint(o_l, o_r))
+    RAND_X = str(random.randint(xy_l, xy_r))
+    RAND_Y = str(random.randint(xy_l, xy_r))
+    TEST_STR = CMD + PARAM_O + RAND_O + PARAM_X + RAND_X + PARAM_Y + RAND_Y
 
-print ("--------------------------------")
-print ("1_1_jump_ball too many params")
-os.system("python3 " + TEST1_1 + " 1 2 3 4 5")
+    print(PARAM_O + RAND_O + PARAM_X + RAND_X + PARAM_Y + RAND_Y)
+    os.system(TEST_STR)
+    print("")
 
-o = " o="
-x = " x="
-y = " y="
-print ("--------------------------------")
-print ("1_1_jump_ball 1. a hold ball")
+
+PY3 = "python3 "
+WORKSPACE = "/Users/syd/PycharmProjects/python_little_games/"
+TEST1_1 = "lesson1_1_jump_ball.py"
+CMD = PY3 + WORKSPACE + TEST1_1
+
+print("********** test begin **********")
+
+print("1_1_jump_ball no params")
+os.system(CMD)
+
+print("--------------------------------")
+print("1_1_jump_ball too many params")
+os.system(CMD + " 1 2 3 4 5")
+
+PARAM_O = " o="
+PARAM_X = " x="
+PARAM_Y = " y="
+print("--------------------------------")
+print("1_1_jump_ball 1. a hold ball")
+
+for i in range(5):
+    test(1, 100, 1, 10)
+
+for i in range(10):
+    test(-50, 150, -5, 15)
