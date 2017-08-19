7e6da6ee6e4f9223f20727bec856ba633f1dc1c5
Syd <324760805@qq.com>
2017-08-19 12:15:06 +0800

    1

--- 1_1_jump_ball.py
+++ 1_1_jump_ball.py
@@ -1,9 +1,29 @@
+#!/usr/bin/env python3
 import os
 import sys
-if len(sys.argv) == 0:
-    print "1.1 jump_ball by syd"
-    print "usage: 1_1_jump_ball.py o=1 x=5 y=10"
-    print "o: "
-    print "     1. a hold ball"
-    print "x: the x coordinate"
-    print "y: the y coordinate"
+
+
+# print len(sys.argv)
+# print sys.argv
+
+def print_help():
+    print ("1.1 jump_ball by syd")
+    print ("usage: 1_1_jump_ball.py o=1 x=5 y=10")
+    print ("o: ")
+    print ("     1. a hold ball")
+    print ("x: the x coordinate (5 default)")
+    print ("y: the y coordinate (10 default)")
+
+
+if len(sys.argv) == 1:
+    print_help()
+    exit(1)
+
+x = 5
+y = 10
+o = 1
+
+if len(sys.argv) > 4:
+    print ("too many argvs")
+    print_help()
+    exit(5)
