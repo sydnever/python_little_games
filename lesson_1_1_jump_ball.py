66ea403a271fd513102e94bcf6800acfb23ba899
Syd <324760805@qq.com>
2017-08-19 15:17:35 +0800

    hold ball begin

---
+++ lesson_1_1_jump_ball.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+    Lesson 1.1 A jump ball
+"""
+import os
+import sys
+
+
+# print len(sys.argv)
+# print sys.argv
+
+def print_help():
+    """
+        help
+    """
+    print("1.1 jump_ball by syd")
+    print("usage: 1_1_jump_ball.py o=1 x=5 y=10")
+    print("o: ")
+    print("     1. a hold ball")
+    print("x: the x coordinate (5 default)")
+    print("y: the y coordinate (10 default)")
+
+
+if len(sys.argv) == 1:
+    print_help()
+    exit(1)
+
+coordinate_x = 5
+coordinate_y = 10
+option_o = 1
+
+if len(sys.argv) > 4:
+    print("too many argvs")
+    print_help()
+    exit(5)
+
+for tmp_argv in sys.argv:
+    if tmp_argv[0:1] == "o=":
+        option_o = tmp_argv[2:]
+    elif tmp_argv[0:1] == "x=":
+        coordinate_x = tmp_argv[2:]
+    elif tmp_argv[0:1] == "y=":
+        coordinate_y = tmp_argv[2:]
+    else:
+        print("invalid param: " + tmp_argv)
+        exit(2)
+        
--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 import os
 
-test1_1 = "/Users/syd/PycharmProjects/python_little_games/1_1_jump_ball.py"
+TEST1_1 = "/Users/syd/PycharmProjects/python_little_games/1_1_jump_ball.py"
 print ("********** test begin **********")
 
 print ("1_1_jump_ball no params")
-os.system("python3 " + test1_1)
+os.system("python3 " + TEST1_1)
 
 print ("--------------------------------")
 print ("1_1_jump_ball too many params")
-os.system("python3 " + test1_1 + " 1 2 3 4 5")
+os.system("python3 " + TEST1_1 + " 1 2 3 4 5")
 
 o = " o="
 x = " x="
