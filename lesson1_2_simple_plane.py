ab186dcf2e16485b1b1abe229bcbb67c30992b02
Syd <324760805@qq.com>
2017-08-22 22:29:04 +0800

    create 1.2

---
+++ lesson1_2_simple_plane.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Lesson 1.3 A simple plane game
+"""
+import os
+
+
+def simple_plane():
+    """
+        A simple plane game
+    """
+    x = 5
+    y = 10
+
+    while True:
+        os.system("reset")
+
+
+if __name__ == "__main__":
+    simple_plane()
